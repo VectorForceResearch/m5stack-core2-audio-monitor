122b422d3f8afed65285c3e205fee7ce2481063f
JL <marsmen@hotmail.com>
2022-11-18 14:36:16 -0800

    Updated issue with I2S

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ i2s_config_t i2s_config = {
     .sample_rate = 64000,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
     .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 2,
     .dma_buf_len = 1024,
@@ -23,7 +23,8 @@ i2s_pin_config_t i2s_pins = {
     .bck_io_num = GPIO_NUM_12,
     .ws_io_num = GPIO_NUM_0,
     .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = GPIO_NUM_34};
+    .data_in_num = GPIO_NUM_34
+    };
 
 Application *application;
 
@@ -38,6 +39,9 @@ void setup()
   );
   M5.Axp.SetLcdVoltage(3000);
   M5.Lcd.fillScreen(TFT_BLACK);
+  // M5Core grabs I2S_NUM_0 so let's get it back
+  i2s_driver_uninstall(I2S_NUM_0);
+
   // create our application
   application = new Application(M5.Lcd, M5.Touch, WINDOW_SIZE);
   application->begin(i2s_config, i2s_pins);
